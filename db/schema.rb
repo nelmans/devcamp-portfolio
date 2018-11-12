b89790d7e1420d21d00c21264369196c79f1ada3
nelmans <nelmansena@gmail.com>
2018-11-12 17:23:40 +0100

    Created seed file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_10_110906) do
-=======
 ActiveRecord::Schema.define(version: 2018_11_10_114056) do
->>>>>>> model-generator
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +32,6 @@ ActiveRecord::Schema.define(version: 2018_11_10_114056) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
@@ -45,5 +39,4 @@ ActiveRecord::Schema.define(version: 2018_11_10_114056) do
     t.datetime "updated_at", null: false
   end
 
->>>>>>> model-generator
 end
